e64471d3900b44c8e7f9d3c118cff3e07b5e1778
Oscar Pérez <oscarperez0196@gmail.com>
2015-10-18 08:25:38 -0500

    Adding calculator.h

---
+++ calculator.h
@@ -0,0 +1,66 @@
+class Calculator
+{
+public:
+	Calculator();
+	~Calculator();
+	double add();
+	double subtract();
+	double multiply();
+	double divide();
+	/*getters & setters*/
+	double getFirstNumber();
+	void   setFirstNumber(double);
+	double getSecondNumber();
+	void   setSecondNumber(double);
+private:
+	double firstNumber;
+	double secondNumber;
+};
+
+Calculator::Calculator() {}
+
+Calculator::~Calculator() {}
+
+/*operations*/
+
+double Calculator::add()
+{
+	return this->getFirstNumber() + this->getSecondNumber();
+}
+
+double Calculator::subtract()
+{
+	return this->getFirstNumber() - this->getSecondNumber();
+}
+
+double Calculator::multiply()
+{
+	return this->getFirstNumber() * this->getSecondNumber();
+}
+
+double Calculator::divide()
+{
+	return this->getFirstNumber() / this->getSecondNumber();
+}
+
+/*getters & setters*/
+
+double Calculator::getFirstNumber()
+{
+	return this->firstNumber;
+}
+
+void Calculator::setFirstNumber(double firstNumber)
+{
+	this->firstNumber = firstNumber;
+}
+
+double Calculator::getSecondNumber()
+{
+	return this->secondNumber;
+}
+
+void Calculator::setSecondNumber(double secondNumber)
+{
+	this->secondNumber = secondNumber;
+}
