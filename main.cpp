2c76e438e6b01eeb5064f55c7ab89a6998ad3c2b
Oscar Pérez <oscarperez0196@gmail.com>
2015-10-18 08:18:56 -0500

    It has created the Calculator class

--- main.cpp
+++ main.cpp
@@ -2,8 +2,76 @@
 
 using namespace std;
 
+class Calculator
+{
+public:
+	Calculator();
+	~Calculator();
+	double add();
+	double subtract();
+	double multiply();
+	double divide();
+	/*getters & setters*/
+	double getFirstNumber();
+	void   setFirstNumber(double);
+	double getSecondNumber();
+	void   setSecondNumber(double);
+private:
+	double firstNumber;
+	double secondNumber;
+};
+
+Calculator::Calculator() {}
+
+Calculator::~Calculator() {}
+
+/*operations*/
+
+double Calculator::add()
+{
+	return this->getFirstNumber() + this->getSecondNumber();
+}
+
+double Calculator::subtract()
+{
+	return this->getFirstNumber() - this->getSecondNumber();
+}
+
+double Calculator::multiply()
+{
+	return this->getFirstNumber() * this->getSecondNumber();
+}
+
+double Calculator::divide()
+{
+	return this->getFirstNumber() / this->getSecondNumber();
+}
+
+/*getters & setters*/
+
+double Calculator::getFirstNumber()
+{
+	return this->firstNumber;
+}
+
+void Calculator::setFirstNumber(double firstNumber)
+{
+	this->firstNumber = firstNumber;
+}
+
+double Calculator::getSecondNumber()
+{
+	return this->secondNumber;
+}
+
+void Calculator::setSecondNumber(double secondNumber)
+{
+	this->secondNumber = secondNumber;
+}
+
+/*main method*/
 int main(int argc, char const *argv[])
 {
-	
+
 	return 0;
 }
